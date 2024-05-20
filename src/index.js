af56a103ecdac6e0840c74f27fc7bfc2f97d8399
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-05-20 06:00:05 -0700

    addedbsecond method of db connection

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ dotenv.config({
 });
 connectDB();
 
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//   } catch (error) {
-//     console.error("ERROR: ", error);
-//     throw err;
-//   }
-// })();
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+*/
